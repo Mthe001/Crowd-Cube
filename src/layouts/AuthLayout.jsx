dc3505f6b573bdecb9901f196e1d6ee38409670c
MThe001 <mdredwanulhaque01614591672@gmail.com>
2024-12-04 03:58:57 +0600

    fix:add theme-context - autlayout

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+
+import React, { useContext } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { ThemeContext } from '../provider/AuthProvider';  // Import ThemeContext
 
 const AuthLayout = () => {
+    // Get the current theme from ThemeContext
+    const { theme } = useContext(ThemeContext);
+
     return (
-        <div className="auth-layout">
+        <div className={`auth-layout ${theme === 'light' ? 'bg-white' : 'bg-base-900'}`}>
             <Navbar />
 
-            <main className="auth-main">
+            <main className={`auth-main ${theme === 'light' ? 'text-black' : 'text-white'}`}>
                 <div className="content">
                     {/* The authentication pages (login, register) will be rendered here */}
                     <Outlet />
@@ -21,3 +26,4 @@ const AuthLayout = () => {
 };
 
 export default AuthLayout;
+
