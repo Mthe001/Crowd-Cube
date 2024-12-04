c9111ee57f67c0ab0464eb8a035d107aa4006be2
MThe001 <mdredwanulhaque01614591672@gmail.com>
2024-12-04 08:53:29 +0600

    fix:secure-firebase.config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,15 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Get Firebase config from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyAu-bUj_exTRd-c-tGrHh9NfQ0Jh5mJy9c",
-  authDomain: "assginment-10-6b155.firebaseapp.com",
-  projectId: "assginment-10-6b155",
-  storageBucket: "assginment-10-6b155.firebasestorage.app",
-  messagingSenderId: "921143666906",
-  appId: "1:921143666906:web:5edbb69a8dd6757cca1de3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
