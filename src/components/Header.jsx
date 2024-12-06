0df0ea242c9672d7158d34beefc2784eca1c3215
MThe001 <mdredwanulhaque01614591672@gmail.com>
2024-12-06 18:01:22 +0600

    fix:header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Typewriter } from 'react-simple-typewriter';  // Import Typewriter
 import Carousel from './Carousel';
 
 const Header = () => {
@@ -6,13 +7,42 @@ const Header = () => {
         <div className="text-center py-10">
             {/* Main title with responsive text sizes */}
             <h1 className="font-bold text-3xl sm:text-4xl md:text-4xl lg:text-5xl">
-                Crowd<span className="text-orange-500">cube</span> : A Crowd
-                <span className="text-orange-500">Funding Application</span>
+                <span className="text-orange-500">
+                    <Typewriter
+                        words={['Crowd', 'Cube']}
+                        loop={0}
+                        cursor
+                        cursorStyle="_"
+                        typeSpeed={120}
+                        deleteSpeed={60}
+                        delaySpeed={1000}
+                    />
+                </span>{' '}
+                : A Crowd{' '}
+                <span className="text-orange-500">
+                    <Typewriter
+                        words={['Funding Application']}
+                        loop={1}  // Loop only once
+                        cursor
+                        cursorStyle="_"
+                        typeSpeed={100}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+                    />
+                </span>
             </h1>
 
-            {/* Subtitle with responsive text sizes and padding */}
+            {/* Subtitle with typewriter effect */}
             <h5 className="py-6 sm:py-8 w-[80%] mx-auto font-semibold text-lg sm:text-xl md:text-2xl lg:text-3xl">
-                A crowdfunding website is a platform where people can raise money for different projects, ideas, or causes by inviting others to contribute financially.
+                <Typewriter
+                    words={['A crowdfunding website where people can raise money for different projects, ideas, or causes by inviting others to contribute financially.']}
+                    loop={0}
+                    cursor
+                    cursorStyle="_"
+                    typeSpeed={80}
+                    deleteSpeed={30}
+                    delaySpeed={1000}
+                />
             </h5>
 
             {/* Carousel component */}
